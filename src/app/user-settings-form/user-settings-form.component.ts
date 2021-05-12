617d94eab5a8f13150063cef46ed8a1e6496ff41
Salah-Eddine Nourelayne <senourelayne@cegid.com>
2021-05-12 17:29:04 +0200

    Angular Forms 30%

---
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { UserSettings } from '../data/user-settings';
+
+@Component({
+  selector: 'app-user-settings-form',
+  templateUrl: './user-settings-form.component.html',
+  styleUrls: ['./user-settings-form.component.css']
+})
+export class UserSettingsFormComponent implements OnInit {
+
+  originalUserSettings : UserSettings = {
+      name:null,
+      emailOffers: null,
+      interfaceStyle: null,
+      subscriptionType: null,
+      notes: null
+  }
+
+  userSettings: UserSettings = {...this.originalUserSettings};
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
