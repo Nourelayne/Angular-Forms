ce64b64b818c139e3cecc499bc7f3735ce199296
Salah-Eddine Nourelayne <senourelayne@cegid.com>
2021-05-17 17:20:13 +0200

    Http Service Added

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { DataService } from '../data/data.service';
 import { UserSettings } from '../data/user-settings';
 
 @Component({
@@ -8,23 +10,44 @@ import { UserSettings } from '../data/user-settings';
   styleUrls: ['./user-settings-form.component.css']
 })
 export class UserSettingsFormComponent implements OnInit {
+      postError = false;
+      postErrorMessage = '';
+      subscriptionTypes: Observable<string[]>;
 
       originalUserSettings : UserSettings = {
-      name:null,
-      emailOffers: null,
-      interfaceStyle: null,
-      subscriptionType: null,
-      notes: null
-  }
+          name:null,
+          emailOffers: null,
+          interfaceStyle: null,
+          subscriptionType: null,
+          notes: null
+      }
 
   userSettings: UserSettings = {...this.originalUserSettings};
-  constructor() { }
+  constructor(private dataService : DataService) { }
 
   ngOnInit(): void {
+     this.subscriptionTypes = this.dataService.getSubscriptionTypes();
+  }
+
+  onHttpError(errorResponse: any){
+    console.log('error: ', errorResponse);
+    this.postError = true;
+    this.postErrorMessage = errorResponse.error.errorMessage;
   }
 
   onSubmit(form : NgForm){
-     console.log("In onSubmit : ", form.valid);
+     console.log("In onSubmit : ",  form.valid);
+
+     if(form.valid){
+      this.dataService.postUserSettingsForm(this.userSettings).subscribe(
+        result => console.log('Success : ' ,result),
+        error => this.onHttpError(error));
+     }
+     else{
+       this.postError = true;
+       this.postErrorMessage = "Please fix the above errors";
+     }
+  
   }
 
 }
